a61f0485b9cf3a9b724437371481890a1f2d90c1
jptivan53 <jptivan53@gmail.com>
2020-10-11 17:51:48 -0400

    Grafica funcionando

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -1,7 +1,7 @@
+import { fade, makeStyles, useTheme } from "@material-ui/core";
 import { AxisBottom, AxisLeft } from "@visx/axis";
 import { curveMonotoneX } from "@visx/curve";
 import { localPoint } from "@visx/event";
-import { LinearGradient } from "@visx/gradient";
 import { GridColumns, GridRows } from "@visx/grid";
 import { Group } from "@visx/group";
 import { scaleLinear, scaleTime } from "@visx/scale";
@@ -13,14 +13,9 @@ import { timeFormat } from "d3-time-format";
 import React, { useCallback, useMemo } from "react";
 import { PaisResponse, StatusType } from "../types";
 
-//Colors
-export const background = "#3b6978";
-export const background2 = "#204051";
-export const accentColor = "#edffea";
-export const accentColorDark = "#75daad";
 const tooltipStyles = {
   ...defaultStyles,
-  background,
+  background: "#3b6978",
   border: "1px solid white",
   color: "white",
 };
@@ -30,8 +25,7 @@ const formatDate = timeFormat("%b %d, '%y");
 // accessors
 const getXValue = (d: PaisResponse): Date => new Date(d.Date);
 const getYValue = (status: StatusType) => (d: PaisResponse) => d[status];
-
-const bisectDate = bisector<PaisResponse, Date>((p) => new Date(p.Date)).right;
+const bisectDate = bisector<PaisResponse, Date>((p) => new Date(p.Date)).left;
 
 type AreaProps = {
   width: number;
@@ -46,22 +40,20 @@ type MyProps = AreaProps & {
 
 type TooltipData = PaisResponse;
 
-const colors = {
-  Deaths: {
-    main: "#f56565",
-    light: "#e53e3e",
-    dark: "#822727",
+const useStyles = makeStyles((theme) => ({
+  tick: {
+    fill: theme.palette.getContrastText(theme.palette.background.paper),
+    "& text": {
+      fill: theme.palette.getContrastText(theme.palette.background.paper),
+    },
   },
-
-  Confirmed: { main: "#d69e2e", light: "#faf089", dark: "#744210" },
-  Recovered: { main: "#38a169", light: "#9ae6b4", dark: "#22543d" },
-};
+}));
 
 export const Chart = withTooltip<MyProps, TooltipData>(
   ({
     width,
     height,
-    margin = { top: 40, right: 30, bottom: 50, left: 50 },
+    margin = { top: 40, right: 0, bottom: 50, left: 50 },
     showTooltip,
     hideTooltip,
     tooltipData,
@@ -71,6 +63,18 @@ export const Chart = withTooltip<MyProps, TooltipData>(
     status = "Confirmed",
   }: MyProps & WithTooltipProvidedProps<TooltipData>) => {
     if (width < 10) return null;
+
+    const classes = useStyles();
+    const theme = useTheme();
+    const contrastText = theme.palette.getContrastText(
+      theme.palette.background.paper
+    );
+    const colorStatusKey =
+      status === "Confirmed"
+        ? "info"
+        : status === "Recovered"
+        ? "success"
+        : "error";
 
     // bounds
     const xMax = width - margin.left - margin.right;
@@ -95,6 +99,10 @@ export const Chart = withTooltip<MyProps, TooltipData>(
       [data, yMax, status]
     );
 
+    React.useEffect(() => {
+      console.log({ theme });
+    }, [theme]);
+
     // tooltip handler
     const handleTooltip = useCallback(
       (
@@ -102,7 +110,9 @@ export const Chart = withTooltip<MyProps, TooltipData>(
           | React.TouchEvent<SVGRectElement>
           | React.MouseEvent<SVGRectElement>
       ) => {
-        const { x } = localPoint(event) || { x: 0 };
+        // Mouse X pos
+        let { x } = localPoint(event) || { x: 0 };
+        x -= margin.left;
         const x0 = xScale.invert(x);
         const index = bisectDate(data, x0, 1);
         const d0 = data[index - 1];
@@ -115,10 +125,10 @@ export const Chart = withTooltip<MyProps, TooltipData>(
               ? d1
               : d0;
         }
-        console.log({ x, x0, d0, d1, d, test: typeof x0 });
+
         showTooltip({
           tooltipData: d,
-          tooltipLeft: x - margin.left,
+          tooltipLeft: x,
           tooltipTop: yScale(getYValue(status)(d)),
         });
       },
@@ -128,31 +138,22 @@ export const Chart = withTooltip<MyProps, TooltipData>(
     return (
       <div>
         <svg width={width} height={height}>
-          <rect
-            x={0}
-            y={0}
-            width={width}
-            height={height}
-            fill="#EDF2F7"
-            rx={8}
-          />
-          {/* <LinearGradient
-            id="area-background-gradient"
-            from={background}
-            to={background2}
-          /> */}
-          <LinearGradient
-            id="area-gradient"
-            from={accentColor}
-            to={accentColor}
-            toOpacity={0.1}
-          />
           <Group left={margin.left} top={margin.top}>
+            <rect
+              x={0}
+              y={0}
+              width={xMax}
+              height={yMax}
+              fill={theme.palette.background.paper}
+            />
             <GridRows
               scale={yScale}
               width={xMax}
               strokeDasharray="3,3"
-              stroke="#A0AEC0"
+              stroke={fade(
+                theme.palette.getContrastText(theme.palette.background.default),
+                0.8
+              )}
               strokeOpacity={0.3}
               pointerEvents="none"
             />
@@ -160,7 +161,10 @@ export const Chart = withTooltip<MyProps, TooltipData>(
               scale={xScale}
               height={yMax}
               strokeDasharray="3,3"
-              stroke="#A0AEC0"
+              stroke={fade(
+                theme.palette.getContrastText(theme.palette.background.default),
+                0.8
+              )}
               strokeOpacity={0.3}
               pointerEvents="none"
             />
@@ -170,9 +174,9 @@ export const Chart = withTooltip<MyProps, TooltipData>(
               y={(d) => yScale(getYValue(status)(d)) as number}
               yScale={yScale}
               strokeWidth={1}
-              stroke={colors[status].main}
-              fill={colors[status].light}
-              fillOpacity={0.5}
+              stroke={theme.palette[colorStatusKey].main}
+              fill={theme.palette[colorStatusKey].light}
+              fillOpacity={0.3}
               curve={curveMonotoneX}
             />
             <Bar
@@ -192,7 +196,7 @@ export const Chart = withTooltip<MyProps, TooltipData>(
                 <Line
                   from={{ x: tooltipLeft, y: 0 }}
                   to={{ x: tooltipLeft, y: yMax }}
-                  stroke={colors[status].light}
+                  stroke={fade(theme.palette[colorStatusKey].light, 0.5)}
                   strokeWidth={2}
                   pointerEvents="none"
                   strokeDasharray="5,2"
@@ -212,18 +216,26 @@ export const Chart = withTooltip<MyProps, TooltipData>(
                   cx={tooltipLeft}
                   cy={tooltipTop}
                   r={4}
-                  fill={colors[status].main}
+                  fill={theme.palette[colorStatusKey].main}
                   stroke="white"
                   strokeWidth={2}
                   pointerEvents="none"
                 />
               </g>
             )}
-            <AxisLeft scale={yScale} />
+            <AxisLeft
+              scale={yScale}
+              tickClassName={classes.tick}
+              stroke={contrastText}
+              tickStroke={contrastText}
+            />
             <AxisBottom
               top={yMax}
               scale={xScale}
-              numTicks={width > 520 ? 10 : 5}
+              numTicks={10}
+              tickClassName={classes.tick}
+              stroke={contrastText}
+              tickStroke={contrastText}
             />
           </Group>
         </svg>
@@ -237,7 +249,7 @@ export const Chart = withTooltip<MyProps, TooltipData>(
               {`${status}: ${getYValue(status)(tooltipData)}`}
             </Tooltip>
             <Tooltip
-              top={-14}
+              top={0}
               left={tooltipLeft}
               style={{
                 ...defaultStyles,
