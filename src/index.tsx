64e8437520c9e45980f4a67c7ee6f0344a0667e4
jptivan53 <jptivan53@gmail.com>
2020-10-14 21:13:58 -0400

    Import desde @material-ui/core

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { CssBaseline } from "@material-ui/core";
-import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import { ThemeProvider, createMuiTheme } from "@material-ui/core";
 
 const theme = createMuiTheme({
   palette: {
