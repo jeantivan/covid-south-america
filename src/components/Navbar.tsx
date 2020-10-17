5a71f90435c352c6df4b98cf0071ac6e55e4ce3a
jptivan53 <jptivan53@gmail.com>
2020-10-16 20:39:45 -0400

    Cambiar el colorMode

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,20 @@
 import {
-  IconButton,
   AppBar,
+  Box,
+  IconButton,
+  Link,
+  makeStyles,
   Toolbar,
   Typography,
-  Link,
+  useTheme,
 } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
+import Brightness4Icon from "@material-ui/icons/Brightness4";
+import Brightness7Icon from "@material-ui/icons/Brightness7";
 import GithubIcon from "@material-ui/icons/GitHub";
 import { Link as ReachLink } from "@reach/router";
-
 import React from "react";
+import { useColorMode } from "./DarkMode";
+
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
@@ -24,6 +29,14 @@ const useStyles = makeStyles((theme) => ({
 
 export const Navbar: React.FC = () => {
   const classes = useStyles();
+
+  const { colorMode, toogleColorMode } = useColorMode();
+
+  const theme = useTheme();
+
+  React.useEffect(() => {
+    console.log({ type: theme.palette.type });
+  }, [theme]);
   return (
     <AppBar position="static" color="inherit" elevation={0}>
       <Toolbar>
@@ -32,14 +45,25 @@ export const Navbar: React.FC = () => {
             Covid-19 Latinoamerica
           </Link>
         </Typography>
-
-        <IconButton
-          component="a"
-          href="https://github.com/jptivan53/covid-latam"
-          aria-label="Github profile"
-        >
-          <GithubIcon />
-        </IconButton>
+        <Box mr={2}>
+          <IconButton
+            onClick={() => {
+              toogleColorMode();
+            }}
+            aria-label="Toogle color mode"
+          >
+            {colorMode === "light" ? <Brightness4Icon /> : <Brightness7Icon />}
+          </IconButton>
+        </Box>
+        <Box>
+          <IconButton
+            component="a"
+            href="https://github.com/jptivan53/covid-latam"
+            aria-label="Github profile"
+          >
+            <GithubIcon />
+          </IconButton>
+        </Box>
       </Toolbar>
     </AppBar>
   );
