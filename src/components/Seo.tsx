8bb9b31412b3a4d245fec24b7cb0ee0b069d5481
jptivan53 <jptivan53@gmail.com>
2020-10-21 18:13:08 -0400

    Component de seo simple

---
+++ src/components/Seo.tsx
@@ -0,0 +1,32 @@
+import { useTheme } from "@material-ui/core";
+import React from "react";
+import { Helmet } from "react-helmet";
+
+interface SeoProps {
+  title?: string;
+  description?: string;
+}
+
+export const Seo: React.FC<SeoProps> = ({ title, description }) => {
+  const theme = useTheme();
+  return (
+    <Helmet
+      htmlAttributes={{
+        lang: "en",
+      }}
+      title={title}
+      titleTemplate="%s | Covid-19 en Sur América"
+      defaultTitle="Covid-19 en Sur América"
+      meta={[
+        {
+          name: "description",
+          content: description || "Coronavirus cases in South America",
+        },
+        {
+          name: "theme-color",
+          content: theme.palette.primary.main,
+        },
+      ]}
+    />
+  );
+};
