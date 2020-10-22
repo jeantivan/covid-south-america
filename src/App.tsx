74d2a6633a5baffeda3693d3c1d8f4a8c1cd814d
jptivan53 <jptivan53@gmail.com>
2020-10-22 15:26:57 -0400

    Grid Layout y Pagina Error404

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import {
   createMuiTheme,
   CssBaseline,
+  makeStyles,
   responsiveFontSizes,
   ThemeProvider,
 } from "@material-ui/core";
@@ -10,8 +11,40 @@ import { Layout } from "./components/Layout";
 import { CountryPage } from "./pages/CountryPage";
 import { HomePage } from "./pages/HomePage";
 import orange from "@material-ui/core/colors/orange";
+import { Error404Page } from "./pages/Error404Page";
+
+const useStyles = makeStyles({
+  "@global": {
+    "html, body": {
+      width: "100%",
+      height: "100%",
+    },
+    "#root": {
+      height: "100%",
+      display: "grid",
+      gridTemplateColumns: "1fr",
+      gridTemplateRows: "auto 1fr auto",
+      gap: "0px 0px",
+      gridTemplateAreas: `
+        "header"
+        "main"
+        "footer"
+      `,
+    },
+    "#header": {
+      gridArea: "header",
+    },
+    "#main": {
+      gridArea: "main",
+    },
+    "#footer": {
+      gridArea: "footer",
+    },
+  },
+});
 
 function App() {
+  useStyles();
   const { colorMode } = useColorMode();
 
   return (
@@ -28,7 +61,8 @@ function App() {
       <CssBaseline />
       <Layout>
         <HomePage path="/" />
-        <CountryPage path="/country/:country" />
+        <Error404Page path="404" />
+        <CountryPage path="country/:country" />
       </Layout>
     </ThemeProvider>
   );
