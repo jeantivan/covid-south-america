8002234aa56547acc489f28ea8423812a1ee6547
Jean Paul Tivan <jptivan53@gmail.com>
2022-06-06 00:00:56 -0400

    Ajustes para la nueva version de React

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,20 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 
 import App from "./App";
-import { ColorModeProvider } from "./components/ColorMode";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+
+root.render(
   <React.StrictMode>
     <Router>
-      <ColorModeProvider>
-        <App />
-      </ColorModeProvider>
+      <App />
     </Router>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
 
 // If you want your app to work offline and load faster, you can change
