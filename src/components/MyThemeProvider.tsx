4e8a8a56616e71dd203b27a223b3d22f592c22aa
Jean Paul Tivan <jptivan53@gmail.com>
2022-06-05 23:59:20 -0400

    Maneja el cambio de modo Oscuro y la config del tema

---
+++ src/components/MyThemeProvider.tsx
@@ -0,0 +1,79 @@
+import React, {
+  useState,
+  ReactNode,
+  useContext,
+  createContext,
+  useEffect,
+  useMemo,
+} from "react";
+import { ColorModeOptions } from "../types";
+import { getInitialColorMode, COLOR_MODE_KEY } from "../utils";
+
+import {
+  createTheme,
+  responsiveFontSizes,
+  ThemeProvider,
+} from "@mui/material/styles";
+import { CssBaseline } from "@mui/material";
+import { orange } from "@mui/material/colors";
+
+interface ColorModeContext {
+  mode: ColorModeOptions;
+  toggleColorMode: () => void;
+}
+
+const ColorMode = createContext({} as ColorModeContext);
+
+export const useColorMode = () => useContext(ColorMode);
+
+export const MyThemeProvider = ({ children }: { children: ReactNode }) => {
+  const [mode, rawSetColorMode] = useState<"light" | "dark">(
+    getInitialColorMode()
+  );
+
+  const colorMode = useMemo(
+    () => ({
+      toggleColorMode: () => {
+        rawSetColorMode((prevColorMode) => {
+          let nextColorMode: ColorModeOptions =
+            prevColorMode === "light" ? "dark" : "light";
+
+          window.localStorage.setItem(COLOR_MODE_KEY, nextColorMode);
+
+          return nextColorMode;
+        });
+      },
+      mode,
+    }),
+    [mode]
+  );
+
+  const theme = useMemo(
+    () =>
+      responsiveFontSizes(
+        createTheme({
+          palette: {
+            mode,
+            primary: orange,
+          },
+        })
+      ),
+    [mode]
+  );
+
+  useEffect(() => {
+    if (!window.localStorage.getItem(COLOR_MODE_KEY)) {
+      window.localStorage.setItem(COLOR_MODE_KEY, mode);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <ColorMode.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        {children}
+      </ThemeProvider>
+    </ColorMode.Provider>
+  );
+};
